dc2948a16ccf3c4999ee1f3a269ed83db0f74e72
Ekalavya Latchireddi <latchireddiekalavya@gmail.com>
2026-07-04 21:37:03 -0700

    updated the python services

--- Services/pythonServices/merger.py
+++ Services/pythonServices/merger.py
@@ -6,13 +6,70 @@ def printImage(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def changeCameraAngle(img):
+    h, w = img.shape[:2]
+
+    src = np.float32([
+        [0, 0],
+        [w, 0],
+        [0, h],
+        [w, h]
+    ])
+
+    # Simulate camera tilting downward
+    # dst = np.float32([
+    #     [100, 50],
+    #     [w - 100, 0],
+    #     [0, h],
+    #     [w, h - 50]
+    # ])
+
+    # # left camera angle
+    # dst = np.float32([
+    #     [150, 0],
+    #     [w, 50],
+    #     [0, h - 50],
+    #     [w - 100, h]
+    # ])
+
+    # #right camera angle
+    # dst = np.float32([
+    #     [0, 50],
+    #     [w - 150, 0],
+    #     [100, h],
+    #     [w, h - 50]
+    # ])
+
+    #Top-down look
+    dst = np.float32([
+        [80, 80],
+        [w - 80, 80],
+        [0, h],
+        [w, h]
+    ])
+    #
+    # #Dramatic perspective
+    # dst = np.float32([
+    #     [200, 100],
+    #     [w - 200, 50],
+    #     [0, h],
+    #     [w, h]
+    # ])
+
+    matrix = cv2.getPerspectiveTransform(src, dst)
+
+    return cv2.warpPerspective(img, matrix, (w, h))
+def removeGreenScreenOfPath(imgPath):
+    img = cv2.imread(imgPath)
+    return removeGreenScreen(img)
+
 def removeGreenScreen(img):
     FRAME_WIDTH = 800
     FRAME_HEIGHT = 600
     img = cv2.resize(img, (FRAME_WIDTH, FRAME_HEIGHT))
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([35, 40, 40])
-    upper_green = np.array([85, 255, 255])
+    lower_green = np.array([40, 40, 10])
+    upper_green = np.array([75, 255, 255])
     green_mask = cv2.inRange(hsv, lower_green, upper_green)
     non_screen_mask = cv2.bitwise_not(green_mask)
     imgWater = cv2.bitwise_and(img, img, mask=non_screen_mask)
@@ -35,15 +92,22 @@ def mergeStaticImgWithVideo(imagePath, videoPath):
     bowl = cv2.imread(imagePath)
     bowl = removeGreenScreen(bowl)
     video = cv2.VideoCapture(videoPath)
+    pause = False
     while True:
-        success, frame = video.read()
-        if not success:
+        if not pause:
+            success, frame = video.read()
+            if not success:
+                break
+            frame = removeGreenScreen(frame)
+            merged = cv2.add(frame, bowl)
+            #merged[np.all(merged == [0, 0, 0], axis=-1)] = (0,255, 0)
+            #merged = changeCameraAngle(merged)
+            cv2.imshow("Video", merged)
+        key = cv2.waitKey(30) & 0xFF
+        if key == ord('q'):
             break
-        frame = removeGreenScreen(frame)
-        merged = cv2.add(frame, bowl)
-        cv2.imshow("Video", merged)
-        if cv2.waitKey(30) == ord('q'):
-            break
+        elif key == ord(' '):
+            pause = not pause
     video.release()
     cv2.destroyAllWindows()
 
